88933839b94b20931eee585485fecdb10cfd12de
Ase <felixasejunior@gmail.com>
2022-10-11 14:32:30 +0300

    commit all

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import HomeHeader from "./HomeHeader";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { CircleButton, RectButton } from "./Button";
+import { SubInfo } from "./SubInfo";
+
+export {
+  FocusedStatusBar,
+  NFTCard,
+  HomeHeader,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDesc,
+  SubInfo,
+};
